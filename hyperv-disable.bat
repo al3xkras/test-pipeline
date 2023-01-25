9d85c8f788e24fd132c6a8ecab727ffba0fe5177
al3xkras <alexandr.krasovskiy@gmail.com>
2023-01-25 14:09:48 +0100

    add files: hyperv-enable & disable; include jenkins build config (Docker) that was used in the test

---
+++ hyperv-disable.bat
@@ -0,0 +1,24 @@
+@echo off
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+::ENTER YOUR CODE BELOW::   
+
+bcdedit /set hypervisorlaunchtype off
+
+::END OF YOUR CODE::
+
+echo.
+
+
+pause
